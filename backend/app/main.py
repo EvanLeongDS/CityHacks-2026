d75aacb6c0338b3e97f07e6c349f8bebe3fca759
root <root@EvanLaptop.>
2026-02-21 14:02:53 -0500

    test commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+import geopandas as gpd
+import json
 
 app = FastAPI(title="CityHacks Backend")
-
+brookline_rollup_data = gpd.read_file("rollup.geojson")
+brookline_pavements_data = gpd.read_file("pavements.geojson")
 
 @app.get("/")
 def read_root() -> dict[str, str]:
@@ -12,3 +14,8 @@ def read_root() -> dict[str, str]:
 @app.get("/health")
 def health_check() -> dict[str, str]:
     return {"status": "ok"}
+
+# cost estimator function
+def cost_estimator():
+# based on the rating and the area plug everything into open ai api and let open ai figure out the costs
+    pass
